80410ed570cf35d2b1b6d65284429535c1fbfab9
Cristhian Godoy <thianserpa@gmail.com>
2026-06-29 20:03:27 +0200

    Configure Vitest and add basic App.test.tsx component test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import { type ReactNode } from 'react';
  */
 export default function App(): ReactNode {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontFamily: 'sans-serif',
+      }}
+    >
       <h1>SPA CAD</h1>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
